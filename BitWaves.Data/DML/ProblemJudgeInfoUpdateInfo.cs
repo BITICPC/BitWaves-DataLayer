d438a567d36545e31b87d77ed3cc40bd24eb6a86
Lancern <msrlancern@126.com>
2019-12-01 22:40:06 +0800

    Fix: UpdateInfo not converted to UpdateDefinition correctly.

--- BitWaves.Data/DML/ProblemJudgeInfoUpdateInfo.cs
+++ BitWaves.Data/DML/ProblemJudgeInfoUpdateInfo.cs
@@ -11,21 +11,25 @@ namespace BitWaves.Data.DML
         /// <summary>
         /// 获取或设置更新的题目评测模式。
         /// </summary>
+        [Set]
         public Maybe<ProblemJudgeMode> JudgeMode { get; set; }
 
         /// <summary>
         /// 获取或设置更新的内建答案检查器选项。
         /// </summary>
+        [Set]
         public Maybe<BuiltinCheckerOptions?> BuiltinCheckerOptions { get; set; }
 
         /// <summary>
         /// 获取或设置更新的题目时间限制，单位为毫秒。
         /// </summary>
+        [Set]
         public Maybe<int> TimeLimit { get; set; }
 
         /// <summary>
         /// 获取或设置更新的题目内存限制，单位为毫秒。
         /// </summary>
+        [Set]
         public Maybe<int> MemoryLimit { get; set; }
     }
 }
